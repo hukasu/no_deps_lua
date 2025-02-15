24d79794a57ef47df889ddc210af6df409cb5136
Lucas Farias <lucasfnf@gmail.com>
2025-02-15 16:56:21 -0300

    Add `must_use` to some helper types

--- src/program/helper_types.rs
+++ src/program/helper_types.rs
@@ -34,18 +34,21 @@ make_exp_list!(VarList);
 pub type NameList = Vec<Value>;
 pub type TableFields<'a> = Vec<(TableKey<'a>, ExpDesc<'a>)>;
 
+#[must_use = "Contains a list of names that need to be added to constants"]
 #[derive(Debug, Default)]
 pub struct ParList {
     pub names: Vec<Value>,
     pub variadic_args: bool,
 }
 
+#[must_use = "Contains a list of names that need to be added to constants"]
 #[derive(Debug, Default)]
 pub struct FunctionNameList<'a> {
     pub names: Vec<&'a str>,
     pub method: Option<&'a str>,
 }
 
+#[must_use = "Contains a key to index into a table"]
 #[derive(Debug, Clone, PartialEq)]
 pub enum TableKey<'a> {
     Array,
